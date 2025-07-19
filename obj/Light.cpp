1cae49187f83d55d763e943c9e00e92aa68a23b0
rrs6 <rrs6@cin.ufpe.br>
2025-07-18 22:09:30 -0300

    fix: change Light class to return a const value.

--- obj/Light.cpp
+++ obj/Light.cpp
@@ -2,10 +2,10 @@
 
 Light::Light(const Point &o, const RGB &color) : o(o), color(color) {}
 
-RGB Light::getColor() {
+RGB Light::getColor() const {
     return this->color;
 }
 
-Point Light::getO() {
+Point Light::getO() const {
     return this->o;
 }
